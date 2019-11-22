5c263e558a15728b701958603d757651728055ba
Dipanjan Sarkar <q7tnhb>
2019-11-22 18:18:15 +0530

    more changes

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -4,100 +4,90 @@ import { Button } from '@blueprintjs/core';
 import { ipcRenderer } from 'electron';
 import electronStorage from 'electron-json-storage';
 
-async function tokenCheck() {
-  return new Promise(resolve => {
-    electronStorage.has('idToken', (error, hasKey) => {
-      if (error) console.log(error);
-
-      resolve(hasKey);
-    });
-  });
-}
-
 class Landing extends Component {
-  constructor(props) {
-    super(props);
-  }
-  // Delete this function and make a seperate sign up module............................................................................
-  signUp() {
-    ipcRenderer.send('openSignUp', true);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			loggedIn: false
+		};
+	}
+	// Delete this function and make a seperate sign up module............................................................................
+	signUp() {
+		ipcRenderer.send('openSignUp', true);
+	}
 
-  render() {
-    // console.log(this.props);
-    return (
-      <div className="">
-        <div className="navBarPadding" />
-        <div className="landingResize">
-          <div className="landingImage" />
-          <div className="landingOverlay" />
-          <div style={{ position: 'relative' }}>
-            <div className="col-md-1" />
-            <div className="col-md-10 text-center">
-              <div id="landingPadding" />
-              <h1>GamiTask For Windows</h1>
-              <p className="">
-                {' '}
-                "Gamify Your Task Management and Note Taking!!!" - Pencupine
-              </p>
-              <hr />
-              <div>
-                <Link to="/home">
-                  <Button
-                    intent={'primary'}
-                    onClick={() => {
-                      console.log('Continue Offline');
-                      // this.props.toggleNavBar();
-                    }}
-                  >
-                    <div className="col-md-12">Continue Offline</div>
-                  </Button>
-                </Link>
-              </div>
-              <hr />
-              <br />
-              Go Online and Back Up
-              <div
-                className="row md-4 auto"
-                style={{
-                  marginTop: '10px',
-                  marginBottom: '10px'
-                }}
-              >
-                <Link to="/login">
-                  <Button
-                    intent={'primary'}
-                    onClick={() => {
-                      console.log('Log Request');
-                    }}
-                  >
-                    <div className="col-md-12">Log In</div>
-                  </Button>
-                </Link>
-              </div>
-              <div className="row text-center">OR</div>
-              <div
-                className="row md-4 auto"
-                style={{ marginTop: '10px', marginBottom: '10px' }}
-              >
-                {/* <Link to="/signup"> */}
-                {tokenCheck() ? null : (
-                  <Button
-                    intent={'primary'}
-                    onClick={this.signUp}
-                    style={{ paddingLeft: '5px', paddingRight: '5px' }}
-                  >
-                    <div className="col-md-12">Sign Up</div>
-                  </Button>
-                )}
-                {/* </Link> */}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		// console.log(this.props);
+		ipcRenderer.on('redirectToHome', (event, value) => {
+			this.setState({ loggedIn: value });
+		});
+		return (
+			<div className="">
+				<div className="navBarPadding" />
+				<div className="landingResize">
+					<div className="landingImage" />
+					<div className="landingOverlay" />
+					<div style={{ position: 'relative' }}>
+						<div className="col-md-1" />
+						<div className="col-md-10 text-center">
+							<div id="landingPadding" />
+							<h1>GamiTask For Windows</h1>
+							<p className=""> "Gamify Your Task Management and Note Taking!!!" - Pencupine</p>
+							<hr />
+							<div>
+								<Link to="/home">
+									<Button
+										intent={'primary'}
+										onClick={() => {
+											console.log('Continue Offline');
+											// this.props.toggleNavBar();
+										}}
+									>
+										<div className="col-md-12">Continue Offline</div>
+									</Button>
+								</Link>
+							</div>
+							<hr />
+							<br />
+							Go Online and Back Up
+							<div
+								className="row md-4 auto"
+								style={{
+									marginTop: '10px',
+									marginBottom: '10px'
+								}}
+							>
+								<Link to="/login">
+									<Button
+										intent={'primary'}
+										onClick={() => {
+											console.log('Log Request');
+										}}
+									>
+										<div className="col-md-12">Log In</div>
+									</Button>
+								</Link>
+							</div>
+							<div className="row text-center">OR</div>
+							<div className="row md-4 auto" style={{ marginTop: '10px', marginBottom: '10px' }}>
+								{/* <Link to="/signup"> */}
+								{this.state.loggedIn ? null : (
+									<Button
+										intent={'primary'}
+										onClick={this.signUp}
+										style={{ paddingLeft: '5px', paddingRight: '5px' }}
+									>
+										<div className="col-md-12">Sign Up</div>
+									</Button>
+								)}
+								{/* </Link> */}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Landing;
--- src/services/auth/auth.js
+++ src/services/auth/auth.js
@@ -7,104 +7,105 @@ const electronStorage = require('electron-json-storage');
 let signUpChildWindow = null;
 
 admin.initializeApp({
-  credential: admin.credential.cert(
-    require('../../config').firebaseAdminSDK_key
-  ),
-  databaseURL: 'https://testbrowserview.firebaseio.com'
+	credential: admin.credential.cert(require('../../config').firebaseAdminSDK_key),
+	databaseURL: 'https://testbrowserview.firebaseio.com'
 });
 
 // AUTH BROWSER WINDOW GENERATOR-----------------------------
 async function startAuthWindow(mainWindow) {
-  console.log('RENDERING AUTH PAGE');
+	console.log('RENDERING AUTH PAGE');
 
-  signUpChildWindow = new BrowserWindow({
-    parent: mainWindow,
-    width: 400,
-    height: 500,
-    modal: true,
-    show: false,
-    // frame: false,
-    webPreferences: {
-      nodeIntegration: true,
-      webviewTag: true
-    }
-  });
-  console.log('BrowserWindow Generated');
+	signUpChildWindow = new BrowserWindow({
+		parent: mainWindow,
+		width: 400,
+		height: 500,
+		modal: true,
+		show: false,
+		// frame: false,
+		webPreferences: {
+			nodeIntegration: true,
+			webviewTag: true
+		}
+	});
+	console.log('BrowserWindow Generated');
 
-  signUpChildWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, '../../views/auth.html'),
-      protocol: 'file:',
-      slashes: true
-    })
-  );
+	signUpChildWindow.loadURL(
+		url.format({
+			pathname: path.join(__dirname, '../../views/auth.html'),
+			protocol: 'file:',
+			slashes: true
+		})
+	);
 
-  signUpChildWindow.on('ready-to-show', () => {
-    console.log('readyToSHow');
-    signUpChildWindow.show();
-  });
+	signUpChildWindow.on('ready-to-show', () => {
+		console.log('readyToSHow');
+		signUpChildWindow.show();
+	});
 
-  signUpChildWindow.on('closed', function() {
-    signUpChildWindow = null;
-  });
+	signUpChildWindow.on('closed', function() {
+		signUpChildWindow = null;
+	});
 }
 
 // SIGN IN USER----------------------------------------------
 async function signInUser(idToken, mainWindow) {
-  console.log('\n SIGNING IN USER WITH TOKEN');
-  await saveToken('idToken', { idToken: idToken });
-  checkAuthState(mainWindow);
+	console.log('\n SIGNING IN USER WITH TOKEN');
+	await saveToken('idToken', { idToken: idToken });
+	checkAuthState(mainWindow);
 }
 
 // SIGN OUT USER---------------------------------------------
 async function signOutUser() {
-  removeToken('idToken');
+	if (signUpChildWindow !== null) removeToken('idToken');
 }
 
 // AUTH STATE CHECK-------------------------------------------
 async function checkAuthState(mainWindow) {
-  console.log('\n CHECKING AUTH STATE');
+	console.log('\n CHECKING AUTH STATE');
 
-  const data = await getToken('idToken');
-  console.log('\n Undefined or not : ', data);
+	const data = await getToken('idToken');
+	console.log('\n Undefined or not : ', data);
 
-  if (data.idToken !== undefined) {
-    if (await checkTokenState(data.idToken)) {
-      console.log('\n CHECK RESULT : USER IN SESSION');
-      // CLOSE AUTH WINDOW
-      if (signUpChildWindow !== null) signUpChildWindow.hide();
-      mainWindow.webContents.send('redirectToHome', null);
-    } else {
-      console.log('\nCHECK RESULT : USER SESSION EXPIRED. REMOVING ID');
-      removeToken('idToken');
-      if (signUpChildWindow === null) startAuthWindow(mainWindow);
-      signUpChildWindow.webContents.send('signOutUser', true);
-    }
-    return;
-  } else {
-    if (signUpChildWindow === null) startAuthWindow(mainWindow);
-    signUpChildWindow.webContents.send('signOutUser', true);
-  }
+	if (data.idToken !== undefined) {
+		if (await checkTokenState(data.idToken)) {
+			console.log('\n CHECK RESULT : USER IN SESSION');
+			// CLOSE AUTH WINDOW
+			if (signUpChildWindow !== null) signUpChildWindow.hide();
+			mainWindow.webContents.send('redirectToHome', true);
+		} else {
+			console.log('\nCHECK RESULT : USER SESSION EXPIRED. REMOVING ID');
+			removeToken('idToken');
+			if (signUpChildWindow === null) startAuthWindow(mainWindow);
+			signUpChildWindow.webContents.send('signOutUser', true);
+			mainWindow.webContents.send('redirectToHome', false);
+		}
+		return;
+	} else {
+		console.log('TOKEN NOT FOUND');
+		if (signUpChildWindow === null) startAuthWindow(mainWindow);
+		signUpChildWindow.webContents.send('signOutUser', true);
+		mainWindow.webContents.send('redirectToHome', false);
+	}
 }
 
 // CHECK TOKEN STATE---------------------------------------------
 async function checkTokenState(idToken) {
-  console.log('\n CHECKING TOKEN STATE');
-  // console.log(idToken);
-  return new Promise(resolve => {
-    admin
-      .auth()
-      .verifyIdToken(idToken, true)
-      .then(decodedData => {
-        const currentDate = new Date().getTime() / 1000;
-        const authState = !(currentDate > decodedData.exp);
-        console.log('\n authState : ', authState);
-        resolve(authState);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  });
+	console.log('\n CHECKING TOKEN STATE');
+	// console.log(idToken);
+	return new Promise(resolve => {
+		admin
+			.auth()
+			.verifyIdToken(idToken, true)
+			.then(decodedData => {
+				const currentDate = new Date().getTime() / 1000;
+				const authState = !(currentDate > decodedData.exp);
+				console.log('\n authState : ', authState);
+				resolve(authState);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	});
 }
 
 // async function checkToken(idToken) {
@@ -133,39 +134,40 @@ async function checkTokenState(idToken) {
 
 // GET TOKEN--------------------------------------------------
 async function getToken(key) {
-  return new Promise(resolve => {
-    electronStorage.get(key, (error, data) => {
-      if (error) console.log(error);
-      console.log(`\nFETCHING ${key} FROM STORAGE`);
-      resolve(data);
-    });
-  });
+	return new Promise(resolve => {
+		electronStorage.get(key, (error, data) => {
+			if (error) console.log(error);
+			console.log(`\nFETCHING ${key} FROM STORAGE`);
+			resolve(data);
+		});
+	});
 }
 
 // SAVE TOKEN--------------------------------------------------
 async function saveToken(key, value) {
-  console.log(`\nSAVING ${key} TO STORAGE`);
-  return new Promise(resolve => {
-    electronStorage.set(key, value, error => {
-      if (error) throw error;
-    });
-    resolve(true);
-  });
+	console.log(`\nSAVING ${key} TO STORAGE`);
+	return new Promise(resolve => {
+		electronStorage.set(key, value, error => {
+			if (error) throw error;
+		});
+		resolve(true);
+	});
 }
 
 // REMOVE TOKEN--------------------------------------------------
 async function removeToken(key) {
-  console.log(`\nDELETING ${key} FROM STORAGE`);
-  return new Promise(resolve => {
-    electronStorage.remove(key, error => {
-      if (error) throw error;
-    });
-    resolve(true);
-  });
+	console.log(`\nDELETING ${key} FROM STORAGE`);
+	return new Promise(resolve => {
+		electronStorage.remove(key, error => {
+			if (error) throw error;
+		});
+		resolve(true);
+	});
 }
 
 // EXPORTS
 exports.removeToken = removeToken;
+exports.getToken = getToken;
 exports.checkAuthState = checkAuthState;
 exports.startAuthWindow = startAuthWindow;
 exports.signInUser = signInUser;
