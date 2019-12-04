8890d0b5ffa171c2be4b9c4e3b6d340f7a8a4236
Dipanjan Sarkar <q7tnhb>
2019-12-04 12:33:03 +0530

    Removed Auth Reducers

--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -1,5 +1,2 @@
-export const FETCH_AUTH = 'FETCH_AUTH';
-export const AUTH_USER = 'AUTH_USER';
-
 export const SHOW_SIDE_MENU = 'SHOW_SIDE_MENU';
 export const HIDE_SIDE_MENU = 'HIDE_SIDE_MENU';
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import authReducer from './authReducer';
 import veiwReducer from './viewReducer';
 
 export default combineReducers({
